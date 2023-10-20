81f93138dab142a35aac46716f1f79629c00127f
王彬翰 <wangbinhan@baicizhan.com>
2023-10-20 10:45:01 +0800

    13

---
+++ p13_test.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+from p13 import Solution
+
+
+class Test13(TestCase):
+    def test_roman_to_int(self):
+        cases = [
+            {
+                "args": ["III"],
+                "kwargs": {},
+                "expect": 3
+            },
+            {
+                "args": ["IV"],
+                "kwargs": {},
+                "expect": 4
+            },
+            {
+                "args": ["IX"],
+                "kwargs": {},
+                "expect": 9
+            },
+            {
+                "args": ["LVIII"],
+                "kwargs": {},
+                "expect": 58
+            },
+            {
+                "args": ["MCMXCIV"],
+                "kwargs": {},
+                "expect": 1994
+            },
+        ]
+        for index, case in enumerate(cases):
+            result = Solution().romanToInt(*case["args"], **case["kwargs"])
+            expect = case["expect"]
+            self.assertEqual(result, expect, f"case {index} failed")
